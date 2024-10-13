feb907b218ee993b3b356d71c06d4fc885ba8d92
jcallejo <jcallejom@yahoo.es>
2024-10-14 00:08:49 +0200

    subida inicial

---
+++ prototypebff-service/src/main/java/com/archetype/architectural/order/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.archetype.architectural.order.config;
+
+//@Configuration
+public class OpenApiConfig {
+	
+//	@Bean 
+//	public OpenAPI customOpenAPI() {
+//	     return new OpenAPI()
+//	          .components(new Components())
+//	          .info(new Info()
+//	          .title("order API")
+//	          .version("1.0.0"));
+//	          .components(new Components().addSecuritySchemes("bearer-key", 
+//          	  	  new SecurityScheme().type(SecurityScheme.Type.HTTP)
+//          	  	  .scheme("bearer").bearerFormat("JWT"))).addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+//	}
+	
+//	@Bean
+//	public OpenAPI apiInfo() {
+//	
+//	        return new OpenAPI()
+//	                .info(new Info().title("Customer")
+//	                .description("Customer API")
+//	                .version("v1.0.0")).components(new Components()
+//	          	          .addSecuritySchemes("bearer-key", 
+//	          	  	          new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))) 
+//	                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+//	}
+	
+	
+}
