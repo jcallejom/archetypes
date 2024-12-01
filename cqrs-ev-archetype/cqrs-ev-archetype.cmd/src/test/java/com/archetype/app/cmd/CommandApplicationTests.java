35d76616bb6a1e2f2163d23bff537caa1caa6c62
jcallejom <jcallejom@yahoo.es>
2024-12-01 18:44:42 +0100

    cqrs commman mongo emmbeded

--- cqrs-ev-archetype/cqrs-ev-archetype.cmd/src/test/java/com/archetype/app/cmd/CommandApplicationTests.java
+++ cqrs-ev-archetype/cqrs-ev-archetype.cmd/src/test/java/com/archetype/app/cmd/CommandApplicationTests.java
@@ -3,12 +3,15 @@ package com.archetype.app.cmd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CommandApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
+		
 	}
 
 }
---
+++ cqrs-ev-archetype/cqrs-ev-archetype.cmd/src/test/java/com/archetype/app/cmd/api/controllers/OpenPrototypeControllerTest.java
@@ -0,0 +1,80 @@
+package com.archetype.app.cmd.api.controllers;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.archetype.app.cmd.api.command.OpenPrototypeCommand;
+import com.archetype.app.cmd.api.vo.PrototypeResponse;
+import com.archetype.app.common.vo.PrototypeNumberVo;
+import com.archetype.app.common.vo.PrototypeType;
+import com.archetype.cqrsev.core.infrastructure.CommandDispatcher;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
+public class OpenPrototypeControllerTest {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@MockBean
+	private CommandDispatcher commandDispatcher; 
+	
+	@BeforeEach
+	public void setup () {
+		MockitoAnnotations.openMocks(this);
+		this.mockMvc=MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void openPrototypeTest() throws Exception {
+		OpenPrototypeCommand command = OpenPrototypeCommand.builder()
+				.id("1234abc")
+				.prototypeCode("cod1")
+				.prototypeDay("24/05/1995")
+				.prototypeNumbers(
+						List.of(PrototypeNumberVo.builder().type(PrototypeType.E).value(1).build()))
+				.build();
+		PrototypeResponse expectedResponse=new PrototypeResponse("The prototype has been saved successfully",command.getPrototypeCode());
+		
+		doNothing().when(commandDispatcher).send(command);
+//	final ResultActions result=	
+			mockMvc.perform(
+				post("/v1/prototype/openprototype")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(command))
+				)
+				.andExpect(status().isCreated())
+				.andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(expectedResponse)))
+				.andReturn();
+//	result.andExpect(status().isCreated());
+//	result.andExpect(jsonPath("$[*].name", containsInAnyOrder(expectedResponse)));
+	
+		
+	}
+}
